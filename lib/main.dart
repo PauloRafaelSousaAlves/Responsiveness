5a93bf23822669bd989cee043fdf644302de53fb
Paulo Rafael de Sousa Alves <paulo.alves.vortigo@superdigital.com.br>
2023-04-10 17:28:00 -0300

    Column and row

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:responsiveness/responsividade_media_query.dart';
+import 'package:responsiveness/responsividade_column_row.dart';
+// import 'package:responsiveness/responsividade_media_query.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'Flutter Web',
       debugShowCheckedModeBanner: false,
-      home: ResponsividadeMediaQuery(),
+      // home: ResponsividadeMediaQuery(),
+      home: ResponsividadeColumnAndRow(),
     ),
   );
 }
---
+++ lib/responsividade_column_row.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ResponsividadeColumnAndRow extends StatefulWidget {
+  const ResponsividadeColumnAndRow({Key? key}) : super(key: key);
+
+  @override
+  State<ResponsividadeColumnAndRow> createState() =>
+      _ResponsividadeColumnAndRowState();
+}
+
+class _ResponsividadeColumnAndRowState
+    extends State<ResponsividadeColumnAndRow> {
+  @override
+  Widget build(BuildContext context) {
+    ///Para ficar uma melhor vizualização vamos criar duas variaveis de largura e altura
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsividade Column and Row'),
+      ),
+
+      ///As propriedades a seguir serve tanto para Row, quanto para Column
+
+      body: Column(
+        children: [
+          ///O Expanded serve para ocupar todo espaço disponivel
+          Expanded(
+            ///no Flex conseguimos definir a proporção que quero que ocupe
+            ///podemos pensar como se fosse 100% ao total e colocamos no flex a porcentagem correspondente que precisamos
+            flex: 20,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+              flex: 60,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 20,
+                    child: Container(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 20,
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 20,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              )),
+          Expanded(
+            flex: 20,
+            child: Container(
+              color: Colors.yellow,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
