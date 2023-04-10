b5ce76dc99ee2c85329c0d20606e6e548df62bf7
Paulo Rafael de Sousa Alves <paulo.alves.vortigo@superdigital.com.br>
2023-04-10 16:05:44 -0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:responsiveness/responsividade_media_query.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Web',
+      debugShowCheckedModeBanner: false,
+      home: ResponsividadeMediaQuery(),
+    ),
+  );
+}
---
+++ lib/responsividade_media_query.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResponsividadeMediaQuery extends StatefulWidget {
+ const ResponsividadeMediaQuery({Key? key}) : super(key: key);
+
+  @override
+  State<ResponsividadeMediaQuery> createState() => _ResponsividadeMediaQueryState();
+}
+
+class _ResponsividadeMediaQueryState extends State<ResponsividadeMediaQuery> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
